2c77721de7d077241a8575521db932f174ad8315
thieryfr <133136290+thieryfr@users.noreply.github.com>
2025-11-01 11:50:21 +0100

    Delete docs/bridge_core.h

--- docs/bridge_core.h
+++
@@ -1,6 +0,0 @@
-
-#pragma once
-#include "tinybms_victron_bridge.h"
-
-bool Bridge_BuildAndBegin(TinyBMS_Victron_Bridge& bridge, BridgeEventSink& sink);
-bool Bridge_CreateTasks(TinyBMS_Victron_Bridge* bridge);
